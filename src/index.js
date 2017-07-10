dafbf4ec22189d5d9151225fe4d9c7f0bd41a00b
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-07-10 17:08:26 +0300

    project config

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SignInComponent from './main/auth/sign-in/sign-in.component'
-
-ReactDOM.render(<SignInComponent />, document.getElementById('root'));
