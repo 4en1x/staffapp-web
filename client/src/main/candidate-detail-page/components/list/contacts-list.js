3732f77c8a3b9403996e4e69cda2ac8462eec3c9
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-07-11 22:34:01 +0300

    renaime communication-list to contacts-list

--- client/src/main/candidate-detail-page/components/list/contacts-list.js
+++ client/src/main/candidate-detail-page/components/list/contacts-list.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {List} from 'semantic-ui-react';
 
-const CommunicationList = (props) => {
+const ContactsList = (props) => {
 
   const contactInfo = {
     id: '1',
@@ -23,4 +23,4 @@ const CommunicationList = (props) => {
   )
 };
 
-export default CommunicationList;
+export default ContactsList;
