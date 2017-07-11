e170284488f1d9ae57a5483a0626e9975170e885
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-07-11 21:41:57 +0300

    add static communication list

---
+++ client/src/main/candidate-detail-page/components/list/communications-list.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {List} from 'semantic-ui-react';
+
+const CommunicationList = (props) => {
+
+  const contactInfo = {
+    id: '1',
+    email: 'smoiseyenko@gmail.com',
+    phone_number: '1234567890',
+    skype: 'sergey_moiseyenko',
+    city: 'Minsk',
+    linked_link: 'http://linked_in'
+  };
+
+  return (
+    <List as="ul">
+      <List.Item as='li'>{contactInfo.email}</List.Item>
+      <List.Item as='li'>{contactInfo.phone_number}</List.Item>
+      <List.Item as='li'>{contactInfo.skype}</List.Item>
+      <List.Item as='li'>{contactInfo.city}</List.Item>
+      <List.Item as='li'>{contactInfo.linked_link}</List.Item>
+    </List>
+  )
+};
+
+export default CommunicationList;
