aabed239df7b07ae28df89b704010da4de29fa99
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-07-11 15:30:18 +0300

    add menu bar component

---
+++ client/src/components/menu/menu.component.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+export default class MenuComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeItem: this.props.items[0]
+    };
+  }
+
+  handleItemClick = (e, { name }) => {
+    if (name === this.state.activeItem) return;
+    this.setState({ activeItem: name });
+    this.props.menuItemClickHandle(name);
+  };
+
+  render() {
+    const { activeItem } = this.state;
+    const items = this.props.items;
+
+    return (
+      <Menu pointing secondary className="menu-component">
+        {items.map(item =>
+          <Menu.Item
+            key={item.toString()}
+            name={item}
+            active={activeItem === item}
+            onClick={this.handleItemClick}
+          />
+        )}
+      </Menu>
+    );
+  }
+}
