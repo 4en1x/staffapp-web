1f9566e85066f705f346d1f156561df970131fbd
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-07-11 15:33:14 +0300

    index.js update

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SignInComponent from "./main/auth/sign-in/sign-in.component";
-import InterviewListItem from './components/list-items/interview-list-item';
+import InterviewListItem from './components/list/list-items/interview-list-item';
+import WorkerPage from './main/worker-page/worker-page.component';
 
-ReactDOM.render(<InterviewListItem />, document.getElementById("root"));
+ReactDOM.render(<WorkerPage />, document.getElementById("root"));
