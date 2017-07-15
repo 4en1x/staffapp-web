f37705c736de84024900487c8d95c0ed2223b965
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-07-16 01:33:53 +0300

    add navigation component

--- client/src/components/list/list.component.jsx
+++ client/src/components/list/list.component.jsx
@@ -1,32 +1,23 @@
 import React from "react";
 import { List } from "semantic-ui-react";
-import InterviewListItem from "./list-items/interview-list-item";
-import VacancyListItem from "./list-items/vacancy-list-item";
 import "./list.css";
 
-// const VACANCY = {
-//   id: "1",
-//   name: "Project",
-//   dateStart: "09.90.2017",
-//   primarySkill: "primarySkill",
-//   status: "status",
-//   location: "location"
-// };
 
 export default class ListComponent extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    // const ListItem = this.props.listItem;
-    const props = this.props;
+    const ListItem = this.props.listItem;
+    const elements = this.props.elements;
+    console.log(elements);
+    console.log(ListItem);
+
     return (
       <List divided className="list-component">
-        {props.interviews.map(interview =>
-          <InterviewListItem interview={interview} key={interview.id} />
-        )
-        // <VacancyListItem vacancy={VACANCY} key={VACANCY.id} />
-        }
+        {elements.map(element => {
+          return <ListItem element={element} key={element.id}/>
+        })}
       </List>
     );
   }
