1a3f4f846781806d40dfe2736bbadc17f5f953f0
4en1x <free_playerq@mail.ru>
2017-07-19 17:11:53 +0300

    changes in config, bug fix

--- server/config.js
+++ server/config.js
@@ -1,7 +1,16 @@
 const defaultConfig = require('./../config.json');
 
+const myDbConfig = {
+  host: process.env.npm_config_host,
+  port: process.env.npm_config_port,
+  user: process.env.npm_config_user,
+  password: process.env.npm_config_password,
+  database: process.env.npm_config_database,
+  multipleStatements: true,
+};
+
 const config = {
-  db: process.env.DATABASE_URL || defaultConfig.db,
+  db: process.env.DATABASE_URL || myDbConfig || defaultConfig.db,
   web: {
     port: process.env.PORT || defaultConfig.web.port,
     frontendOrigin: defaultConfig.web.frontendOrigin,
