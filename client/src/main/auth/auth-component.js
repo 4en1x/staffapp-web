a057dc728c756fd9d14075c2f3d223c9c82a9278
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-07-19 12:05:22 +0300

    add check auth, use local storage for store users

--- client/src/main/auth/auth-component.js
+++ client/src/main/auth/auth-component.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import {Redirect} from 'react-router-dom';
-import axios from 'axios';
-
-const user = {
-  name: 'I',
-  role: 'HR'
-};
+import React from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
 
 function isAuth() {
- return axios.get('isAuth');
+  const user = localStorage.getItem("user");
+  if (user === null) return;
+  return JSON.parse(user);
 }
 
 export default function checkAuth(Component) {
-  return function (props) {
-    if (!user) return <Redirect to='login' />;
-    return <Component {...props} user={user}/>
+  return function(props) {
+    const userFromSession = isAuth();
+    const user = userFromSession || props.location.state;
+    if (!user) return <Redirect to="login" />;
+    return <Component {...props} user={user} />;
   };
 }
