e8e63f78de2de4bbb2dcb409d109312738d776f5
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-07-19 12:14:30 +0300

    add handle feedback input

--- client/src/main/interview-page/interview-page.js
+++ client/src/main/interview-page/interview-page.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import InterviewComponent from './components/interview.component';
+import React from "react";
+import InterviewComponent from "./components/interview.component";
+import { Redirect } from "react-router-dom";
 
-import './interview-page.css';
+import "./interview-page.css";
 
 const data = {
-  id: '1',
+  id: "1",
   skills: ["skill 1", "skill 2", "skill 3", "skill 4"],
   candidate: "Sergey Moiseenko",
   status: "status: Free",
@@ -134,22 +135,32 @@ export default class InterviewPage extends React.Component {
 
     this.state = {
       interview: null,
-      isLoaded: false
+      isLoaded: false,
+      feedbackClicked: false
     };
-
-    console.log('lalala');
   }
 
   componentDidMount() {
-    console.log(this.props.match.params.id);
-    this.setState({interview: data, isLoaded: true});
+    this.setState({ interview: data, isLoaded: true });
   }
 
+  feedbackClicked = () => {
+    this.setState({ feedbackClicked: true });
+  };
+
   render() {
+    const url = this.props.match.url;
+    if (this.state.feedbackClicked) return <Redirect to={`${url}/feedback`} />;
+
     return (
-    <div className="interview-page">
-      {!this.state.isLoaded? <p>Not Loaded</p> : <InterviewComponent interview={this.state.interview} />}
-    </div>
-    )
+      <div className="interview-page">
+        {!this.state.isLoaded
+          ? <p>Not Loaded</p>
+          : <InterviewComponent
+              interview={this.state.interview}
+              feedbackClicked={this.feedbackClicked}
+            />}
+      </div>
+    );
   }
 }
