ce5e845a3a13fc99a0eb335aec4341832d6b4228
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-07-19 12:08:35 +0300

    add route to pages

--- client/src/main/app/app.js
+++ client/src/main/app/app.js
@@ -4,31 +4,35 @@ import HRPage from "../hr-page/hr-page";
 import Header from "../../components/header/header.components";
 import { Route, Switch, Redirect } from "react-router-dom";
 import InterviewPage from "../interview-page/interview-page";
-import CandidatePage from '../candidate-detail-page/candidate-page';
+import CandidatePage from "../candidate-detail-page/candidate-page";
+import TechnicalFeedback from "../add-technical-feedback-page/technical-feedback.js";
 import "../../index.css";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.user = props.user;
   }
 
-  config = () => {
-    return {
-      HR: HRPage,
-      Worker: WorkerPage
-    };
-  };
+  config = () => ({
+    HR: HRPage,
+    Worker: WorkerPage
+  });
 
   render() {
     const config = this.config();
-    const user = this.props.user;
-    console.log(user);
+    const user = this.user;
+    console.log(this.props);
 
     return (
       <div className="root-class">
         <Header user={{ name: user.name, surname: "Surname" }} />
         <Switch>
-          <Route path="/interviews/:id" component={InterviewPage} />
+          <Route exact path="/interviews/:id" component={InterviewPage} />
+          <Route
+            path="/interviews/:id/feedback"
+            component={TechnicalFeedback}
+          />
           <Route path="/candidates/:id" component={CandidatePage} />
           <Route path="/" component={config[user.role]} />
         </Switch>
