1258a82c6b005ad3658b00769fa5310a50eb6abe
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-07-22 23:19:08 +0300

    update feedback page, use redux, redux form

--- client/src/main/add-technical-feedback-page/components/add-technical-feedback-page.component.jsx
+++ client/src/main/add-technical-feedback-page/components/add-technical-feedback-page.component.jsx
@@ -6,139 +6,90 @@ import { Divider, Segment, List } from "semantic-ui-react";
 import "./add-technical-feedback-page.css";
 import FeedbackTechnicalCard from "../../../components/feedback/feedback-technical-card";
 
-const feedbackID = 12345;
-let data;
-   axios.get("/feedbacks/" + feedbackID)
-  .then(function(response) {
-    data = JSON.parse(response);
-  })
-  .catch(function(error) {
-    alert("OOPS something wrong");
-  });
+class FeedbackTechnicalPage extends React.Component{
 
-// const data = {
-//   fields: [
-//     {
-//       id: 11,
-//       name: "C++ level",
-//       typeSkill: "primary",
-//       feedbackId: 5,
-//       type: "tech"
-//     },
-//     {
-//       id: 12,
-//       name: "Some skill one",
-//       typeSkill: "secondary",
-//       feedbackId: 5,
-//       type: "tech"
-//     },
-//     {
-//       id: 13,
-//       name: "some skill two",
-//       typeSkill: "secondary",
-//       feedbackId: 5,
-//       type: "tech"
-//     },
-//     {
-//       id: 14,
-//       name: "Some other skill one",
-//       typeSkill: "other",
-//       feedbackId: 5,
-//       type: "tech"
-//     },
-//     {
-//       id: 15,
-//       name: "Some other skill two",
-//       typeSkill: "other",
-//       feedbackId: 5,
-//       type: "tech"
-//     },
-//     {
-//       id: 16,
-//       name: "Some other skill three",
-//       typeSkill: "other",
-//       feedbackId: 5,
-//       type: "tech"
-//     }
-//   ]
-// };
+  onSubmitClicked = (value) => {
+    console.log(value);
 
-function showResults(values) {
-  let move = 0;
-  let newArray = [];
+    let move = 0;
+    let newArray = [];
 
-  for (var key in values) {
-    values[key].id = data.fields[move].id;
-    newArray.push(values[key]);
-    move++;
+    for (var key in value) {
+      value[key].id = this.props.data.fields[move].id;
+      newArray.push(value[key]);
+      move++;
+    }
+
+     this.props.addFeedback({fields: newArray});
+  };
+
+  render() {
+    const { handleSubmit, data } = this.props;
+
+    return (
+        <form
+          onSubmit={handleSubmit(this.onSubmitClicked)}
+          className="add-technical-feedback-page"
+          >
+          <div className="title-feedback">feedback</div>
+          <Divider />
+          <Segment id="content-data">
+            <div className="labels"> Major skill </div>
+            {data.fields.map(step => {
+              if (step.type_skill === "primary") {
+                return (
+                  <FeedbackTechnicalCard
+                    data={step}
+                    location={"majorTechnology"}
+                    key={data.id}
+                    />
+                );
+              }
+              return null;
+            })}
+
+          <List size="medium">
+            <List.Header className="list-header">Minor skills</List.Header>
+            {data.fields.map((step, move) => {
+              if (step.type_skill === "secondary") {
+                return (
+                  <List.Item key={step.technology}>
+                    <FeedbackTechnicalCard
+                      data={step}
+                      location={"MinorTechology" + move}
+                      key={data.id}
+                    />
+                  </List.Item>
+                );
+              }
+              return null;
+            })}
+          </List>
+
+          <List size="medium">
+            <List.Header className="list-header">Other skills</List.Header>
+            {data.fields.map((step, move) => {
+              if (step.type_skill === "other") {
+                return (
+                  <List.Item key={step.technology}>
+                    <FeedbackTechnicalCard
+                      data={step}
+                      location={"OtherTechology" + move}
+                      key={data.id}
+                    />
+                  </List.Item>
+                );
+              }
+              return null;
+            })}
+          </List>
+        </Segment>
+        <div className="add-feedback-redux-button">
+          <button type="submit">Feedback</button>
+        </div>
+      </form>
+    );
   }
-  console.log(newArray);
-
-  axios.put("feedbacks/feedbackID", {
-      fields: newArray
-    });
-}
-
-const FeedbackTechnicalPage = props => {
-  const { handleSubmit } = props;
-
-  return (
-    <form
-      onSubmit={handleSubmit(showResults)}
-      className="add-technical-feedback-page"
-    >
-      <div className="title-feedback">feedback</div>
-      <Divider />
-      <Segment id="content-data">
-        <div className="labels"> Major skill </div>
-        {data.fields.map(step => {
-          if (step.typeSkill === "primary") {
-            return (
-              <FeedbackTechnicalCard data={step} location={"majorTechnology"} />
-            );
-          }
-          return null;
-        })}
-
-        <List size="medium">
-          <List.Header className="list-header">Minor skills</List.Header>
-          {data.fields.map((step, move) => {
-            if (step.typeSkill === "secondary") {
-              return (
-                <List.Item key={step.technology}>
-                  <FeedbackTechnicalCard
-                    data={step}
-                    location={"MinorTechology" + move}
-                  />
-                </List.Item>
-              );
-            }
-            return null;
-          })}
-        </List>
-
-        <List size="medium">
-          <List.Header className="list-header">Other skills</List.Header>
-          {data.fields.map((step, move) => {
-            if (step.typeSkill === "other") {
-              return (
-                <List.Item key={step.technology}>
-                  <FeedbackTechnicalCard
-                    data={step}
-                    location={"OtherTechology" + move}
-                  />
-                </List.Item>
-              );
-            }
-            return null;
-          })}
-        </List>
-      </Segment>
-      <div className="add-feedback-redux-button">
-        <button type="submit">Feedback</button>
-      </div>
-    </form>
-  );
 };
 
 export default reduxForm({ form: "simple" })(FeedbackTechnicalPage);
--- client/src/main/add-technical-feedback-page/technical-feedback.js
+++ client/src/main/add-technical-feedback-page/technical-feedback.js
@@ -1,26 +1,38 @@
 import React from "react";
 import FeedbackTechnicalCard from "./components/add-technical-feedback-page.component.jsx";
 import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actionCreators from "../../action-creators/action-creators.js";
+import SemanticLoader from "../../components/loaders/semantic-loader.js";
 
-export default class TechnicalFeedback extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isFeedbackSent: false
-    };
+class TechnicalFeedback extends React.Component {
+  componentDidMount() {
+    this.props.getFeedbackFormFields(this.props.match.params.id);
   }
 
   addFeedback = feedback => {
-    this.setState({ isFeedbackSend: true });
+    this.props.putFeedback(this.props.match.params.id, feedback);
   };
 
   render() {
-    if (this.state.isFeedbackSend) return <Redirect to="/" />;
+    if (this.props.isFeedbackUploaded) return <Redirect to="/" />;
 
     return (
       <div>
-        <FeedbackTechnicalCard addFeedback={this.addFeedback} />
+        {!this.props.fields
+          ? <SemanticLoader />
+          : <FeedbackTechnicalCard
+              data={this.props.fields}
+              addFeedback={this.addFeedback}
+            />}
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  fields: state.feedback.feedbackFields,
+  isFeedbackUploaded: state.feedback.isUploaded
+});
+
+export default connect(mapStateToProps, actionCreators)(TechnicalFeedback);
