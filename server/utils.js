8225762c20cda1a5d54e1bcb3e790aebb82df1bd
4en1x <free_playerq@mail.ru>
2017-07-24 21:17:51 +0300

    bug fixes

--- server/utils.js
+++ server/utils.js
@@ -1,3 +1,35 @@
+const forOwn = require('lodash.forown');
+const snakeCase = require('lodash.snakecase');
+const camelCase = require('lodash.camelcase');
+const isPlainObject = require('lodash.isplainobject');
+const isArray = require('lodash.isarray');
+
+function walk(obj, cb) {
+  const x = isArray(obj) ? [] : {};
+
+  forOwn(obj, (v, k) => {
+    if (isPlainObject(v) || isArray(v) || typeof (v) === typeof ({})
+            && v != null && !(v instanceof Date)) {
+      v = walk(v, cb);
+    }
+
+    x[cb(k)] = v;
+  });
+
+  return x;
+}
+
+const toCamel = (obj) => {
+  const newObj = walk(obj, k => camelCase(k));
+  return newObj;
+};
+
+const toSnake = (obj) => {
+  const newObj = walk(obj, k => snakeCase(k));
+
+  return newObj;
+};
+
 function applyDefault(sourceOptions, defaultOptions) {
   return Object.assign({}, defaultOptions, sourceOptions);
 }
@@ -17,4 +49,6 @@ function clearFields(object) {
 module.exports = {
   applyDefault,
   clearFields,
+  toCamel,
+  toSnake,
 };
