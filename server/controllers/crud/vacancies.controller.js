3ee7c8689f1baf955265071b5e3c029c8cb1a186
4en1x <free_playerq@mail.ru>
2017-07-24 23:26:21 +0300

    fix job_start in vacancies

--- server/controllers/crud/vacancies.controller.js
+++ server/controllers/crud/vacancies.controller.js
@@ -1,5 +1,4 @@
 const CRUDController = require('../crud.controller');
-const fecha = require('fecha');
 const db = require('../../dao');
 const service = require('../../services/vacancies.service');
 
@@ -7,13 +6,7 @@ class VacanicesController extends CRUDController {
   constructor() {
     super(db.vacancies);
   }
-  async readOne(req, res) {
-    const onload = async (vacancy) => {
-      vacancy.dateStart = fecha.format(vacancy.jobStart, 'DD/MM/YYYY');
-    };
 
-    await super.readOne(req, res, onload);
-  }
   async create(req, res) {
     const { vacancy, skills, city } = service.createVacancy(req.body);
     await super.create(req, res, { vacancy, skills, city });
