21950147dbab62a8dbabb8d536d377cf5054bec8
Leanid Shutau <the9thr@gmail.com>
2017-07-24 12:15:43 +0300

    Resolve "api-documentation" branch conflicts

--- server/routes/feedbacks.route.js
+++ server/routes/feedbacks.route.js
@@ -1,14 +1,8 @@
 const controllers = require('../controllers/controllers');
 const router = require('express').Router();
 
-<<<<<<< HEAD
-
-router.get('/:id', controller.readFeedback);
-router.put('/:id', controller.updateFeedback);
-=======
 // user + hr + admin section:
 router.get('/:id', controllers.feedbacks.readOne);
 router.post('/:id', controllers.feedbacks.update);
->>>>>>> develop
 
 module.exports = router;
