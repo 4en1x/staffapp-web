99f753e66fc3aae39e9a4b4379693fc230528bdf
NickzZabolotskiy <nickzabolotskiy@gmail.com>
2017-07-28 12:36:22 +0300

    replace foulder location

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
-import SignInComponent from "./main/auth/sign-in/sign-in.component";
-import App from "./main/app/app";
-import checkAuth from './main/auth/auth-component';
-import "./index.css";
 
-ReactDOM.render(
-  <Router>
-      <Switch>
-        <Route path='/login' component={SignInComponent}/>
-        <Route path='/' component={checkAuth(App)}/>
-      </Switch>
-  </Router>,
-  document.getElementById("root")
-);
+import CandidateAddPage from "./main/add-edit-form/vacancy-page/add-vacancy-page";
+
+ReactDOM.render(<CandidateAddPage />, document.getElementById("root"));
