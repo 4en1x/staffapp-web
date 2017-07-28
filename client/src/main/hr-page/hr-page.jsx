c88fa90fd589b33352120d1a1a836a2504879f13
Eugene <hadanyonok@gmail.com>
2017-07-28 14:42:55 +0300

    add AddBtns

--- client/src/main/hr-page/hr-page.jsx
+++ client/src/main/hr-page/hr-page.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import HRNavigationBar from '../../components/hr-navigation-bar/navigation-bar';
-import InterviewListWrapper from './components/interview-list-wrapper';
-import CandidateListWrapper from './components/candidate-list-wrapper';
-import VacancyListWrapper from './components/vacancy-list-wrapper';
-import SecondaryMenuComponent from '../../components/secondary-menu/secondary-menu.component';
-import VacanciesFilterForm from './components/filter/vacancies-filter.container';
-import CandidateFilter from './components/filter/candidates-filter.container';
-import './hr-page.css';
+import React from "react";
+import { Route, Redirect, Link } from "react-router-dom";
+import { Button } from "semantic-ui-react";
+import HRNavigationBar from "../../components/hr-navigation-bar/navigation-bar";
+import InterviewListWrapper from "./components/interview-list-wrapper";
+import CandidateListWrapper from "./components/candidate-list-wrapper";
+import VacancyListWrapper from "./components/vacancy-list-wrapper";
+import SecondaryMenuComponent from "../../components/secondary-menu/secondary-menu.component";
+import VacanciesFilterForm from "./components/filter/vacancies-filter.container";
+import CandidateFilter from "./components/filter/candidates-filter.container";
+import "./hr-page.css";
 
 export default class HRPage extends React.Component {
   render() {
     return (
       <div className="hr-page">
         <HRNavigationBar url="/" activeItem="Interviews" />
-        <div className="hr-page_content">
+        <div className="hr-page_container">
           <Route
             exact
             path={`/`}
@@ -22,16 +23,37 @@ export default class HRPage extends React.Component {
           />
           <Route
             path={`/interviews`}
-            component={() => <InterviewListWrapper />}
+            component={() =>
+              <div className="hr-page_content">
+                <InterviewListWrapper />
+                <Link to="/">
+                  <Button color="twitter">Add</Button>
+                </Link>
+                <SecondaryMenuComponent />
+              </div>}
           />
-          <Route path={`/vacancies`} component={() => <VacancyListWrapper />} />
+          <Route
+            path={`/vacancies`}
+            component={() =>
+              <div>
+                <VacancyListWrapper />
+                <Link to="/">
+                  <Button color="twitter">Add</Button>
+                </Link>
+                <VacanciesFilterForm />
+              </div>}
+          />
           <Route
             path={`/candidates`}
-            component={() => <CandidateListWrapper />}
+            component={() =>
+              <div>
+                <CandidateListWrapper />
+                <Link to="/">
+                  <Button color="twitter">Add</Button>
+                </Link>
+                <CandidateFilter />
+              </div>}
           />
-          /*<SecondaryMenuComponent />*/
-          <VacanciesFilterForm/>
-          <CandidateFilter/>
         </div>
       </div>
     );
