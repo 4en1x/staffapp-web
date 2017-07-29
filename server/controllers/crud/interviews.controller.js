e86ed7ffd1b9687f13f7c1dd07db385282f4aafa
4en1x <free_playerq@mail.ru>
2017-07-29 15:48:23 +0300

    fix fill list request

--- server/controllers/crud/interviews.controller.js
+++ server/controllers/crud/interviews.controller.js
@@ -72,17 +72,12 @@ class InterviewsController extends CRUDController {
 
   async fillLists(req, res) {
     try {
-      if (req.query.type === 'hr') {
-        res.json({
-          hr: await db.skills.find('hr'),
-        });
-      } else {
-        res.json({
-          primary: await db.skills.find('primary'),
-          secondary: await db.skills.find('secondary'),
-          other: await db.skills.find('other'),
-        });
-      }
+      res.json({
+        primary: await db.skills.find('primary'),
+        secondary: await db.skills.find('secondary'),
+        other: await db.skills.find('other'),
+        hr: await db.skills.find('hr'),
+      });
     } catch (err) {
       res.status(500).end();
     }
