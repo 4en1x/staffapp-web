8ca17b1ae39d23499ff62ecc00241cafc8891a99
NickzZabolotskiy <nickzabolotskiy@gmail.com>
2017-08-02 02:13:18 +0300

    add notification

--- client/src/components/header/header.components.js
+++ client/src/components/header/header.components.js
@@ -1,40 +1,58 @@
-import React from 'react';
+import React from "react";
+import { Image, Dropdown, Icon } from "semantic-ui-react";
+import Notification from "../notification/notification";
 import PropTypes from 'prop-types';
-import { Image, Dropdown, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import images from '../../assets/images';
 import './header.css';
 
-const trigger = name => (
+///////////////// messages ////
+const messages = ["message1", "message2", "message3", "message4", "message5"];
+///////////////////////////////
+
+const trigger = name =>
   <span>
     <Icon name="user" /> {name}
-  </span>
-);
+  </span>;
 
 const options = [
-  { value: 'notifications', text: 'Notifications' },
-  { value: 'sign-out', text: 'Sign Out' }
+  { value: "notifications", text: "Notifications" },
+  { value: "sign-out", text: "Sign Out" }
 ];
 
-const DropDownTrigger = (props) =>
-  <Dropdown trigger={trigger(props.user.name)} options={options} onChange={props.itemSelected} />;
-
-export default class HeaderComponent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+class DropDownTrigger extends React.Component {
+  logoutAndNotification = (event, value) => {
+    if (value.value === "sign-out") this.props.itemSelected(event, value);
+    if (value.value === "notifications")
+      document.getElementById("notification-label").style.display = "block";
+  };
 
   render() {
-    const user = this.props.user;
-
     return (
+      <Dropdown
+        trigger={trigger(this.props.user.name)}
+        onChange={this.logoutAndNotification}
+        options={options}
+      />
+    );
+  }
+}
+
+export default class HeaderComponent extends React.Component {
+  render() {
+    const user = this.props.user;
+    return (
+      <div>
       <div className="header-component">
         <Link to="/"><Image className="image" src={images.logo1} /></Link>
         <DropDownTrigger user={user} itemSelected={this.props.itemSelected}/>
       </div>
+          <Notification messages={messages}/>
+      </div>
     );
   }
 }
+
 HeaderComponent.defaultProps = {
   user: {}
 };
