4813d2a31440e0b44b1d16934d9f8311d0dc3c61
NickzZabolotskiy <nickzabolotskiy@gmail.com>
2017-08-02 16:42:10 +0300

    add validation of required fields

--- client/src/components/candidate-add-edit-forms/list/skills-list.js
+++ client/src/components/candidate-add-edit-forms/list/skills-list.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Field } from "redux-form";
-import { List, Dropdown } from "semantic-ui-react";
-import "./skills.css";
+import { List, Dropdown, Label } from "semantic-ui-react";
+import "./skills-list.css";
 import "./candidate.css";
 
 const skillsList = [];
@@ -35,8 +35,9 @@ const secondarySkills = ({ input }) => {
     />
   );
 };
-const englishLevel = ({ input }) => {
-  return (
+const englishLevel = ({ input, meta: { touched, error } }) => {
+    return (
+        <div className="field-with-warning">
     <Dropdown
       placeholder="english level"
       value={input.value}
@@ -51,7 +52,15 @@ const englishLevel = ({ input }) => {
       className="icon"
       icon="graduation"
     />
-  );
+            {touched &&
+            (error &&
+            <div className="warning-block">
+              <Label basic color="red" pointing>
+                Please enter english level
+              </Label>
+            </div>)}
+        </div>
+    );
 };
 
 export default class SkillsListextends extends React.Component {
@@ -84,7 +93,7 @@ export default class SkillsListextends extends React.Component {
 
         <List.Item>
           <div className="item-with-label">
-            english label
+            english level *
             <Field name={"englishLevel"} component={englishLevel} />
           </div>
         </List.Item>
