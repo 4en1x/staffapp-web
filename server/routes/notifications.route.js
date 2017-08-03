fd5789c97431e899470d00e88becf47d20b4dd23
YauheniyaBasaranovich <evgeniabasaranovich@gmail.com>
2017-08-03 15:01:00 +0300

    Fix dates and time in interview text

--- server/routes/notifications.route.js
+++ server/routes/notifications.route.js
@@ -3,8 +3,6 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res) => controllers.notifications.read(req, res));
-router.get('/:id', (req, res) => controllers.notifications.readOne(req, res));
 router.patch('/:id', (req, res) => controllers.notifications.update(req, res));
-router.delete('/:id', (req, res) => controllers.notifications.delete(req, res));
 
 module.exports = router;
--- server/services/notifications.service.js
+++ server/services/notifications.service.js
@@ -1,17 +1,35 @@
 const { date } = require('../utils');
 
+/**
+ * @param {Date} dateTime - interview date
+ * @returns {string} time - interview time not UTC
+ */
+function getTimeString(dateTime) {
+  const hours = `0${dateTime.getHours()}`.slice(-2);
+  const minutes = `0${dateTime.getMinutes()}`.slice(-2);
+  return `${hours}:${minutes}`;
+}
+
+/**
+ * @param {Date} dateTime - interview date
+ * @returns {string} date - interview date not UTC
+ */
+function getDateString(dateTime) {
+  const day = `0${dateTime.getDay()}`.slice(-2);
+  const month = `0${dateTime.getMonth()}`.slice(-2);
+  return `${day}.${month}.${dateTime.getFullYear()}`;
+}
+
+/**
+ * @param {Number | String} userId
+ * @param {Object} interview
+ * @param {Number | String} interviewId
+ * @returns {[Object, Object]} messages
+ */
 function createMessages(userId, interview, interviewId) {
-  const interviewTime = date.getTime(new Date(interview.date));
-  const interviewDate = date.getDate(new Date(interview.date));
+  const interviewTime = getTimeString(new Date(interview.date));
+  const interviewDate = getDateString(new Date(interview.date));
   const interviewDateTime = date.getSQL(new Date(interview.date));
-
-
-  const secondMessage = {
-    date: interviewDateTime,
-    text: `You have one assigned interview today at ${interviewTime}`,
-    userId,
-    interviewId,
-  };
 
   const firstMessage = {
     date: interviewDateTime,
@@ -19,6 +37,13 @@ function createMessages(userId, interview, interviewId) {
     userId,
     interviewId,
     status: 1,
+  };
+
+  const secondMessage = {
+    date: interviewDateTime,
+    text: `You have one assigned interview today at ${interviewTime}`,
+    userId,
+    interviewId,
   };
 
   return [firstMessage, secondMessage];
