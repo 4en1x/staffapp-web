a50728f79c70e50a595a138a3b41672b6c80eb71
Eugene <hadanyonok@gmail.com>
2017-08-03 17:25:45 +0300

    vacancy tab added

--- client/src/main/candidate/candidate-detail-page/components/tabs/info-tab.js
+++ client/src/main/candidate/candidate-detail-page/components/tabs/info-tab.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
 import SkillList from '../lists/skills-list';
 import ContactsList from '../lists/contacts-list';
 import CommunicationsList from '../lists/communications-list';
@@ -8,6 +10,16 @@ const InfoTab = props =>
     <SkillList skills={props.info.skills} />
     <ContactsList contacts={props.info.contacts} />
     <CommunicationsList communication={props.info.communication} />
+    <Link to="/" className=".right">
+      <Button
+        className="add-button"
+        fluid
+        content="Edit"
+        icon="edit"
+        labelPosition="left"
+        color="twitter"
+      />
+    </Link>
   </div>;
 
 export default InfoTab;
---
+++ client/src/main/candidate/candidate-detail-page/components/tabs/vacancies-tab.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const VacanciesTab = props => <div className="tab-content row" />;
+
+export default VacanciesTab;
