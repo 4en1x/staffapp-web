b06aab75c67cab7e71933ca56620de453a3ffe99
Eugene <hadanyonok@gmail.com>
2017-08-07 15:22:09 +0300

    button styles add

---
+++ client/src/components/custom-button/link-button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
+import './custom-button.css';
+
+const LinkButton = props =>
+  <Link to={props.to} className="add-button-container">
+    <Button
+      className="custom-button"
+      fluid
+      content={props.content}
+      icon={props.icon}
+      labelPosition="left"
+      color={props.color}
+    />
+  </Link>;
+
+export default LinkButton;
